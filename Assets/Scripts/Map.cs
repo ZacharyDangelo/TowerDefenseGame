e820765de0ca762d5548e6fbf20222064502c837
Zach <zachdangelo100@gmail.com>
2020-03-07 13:42:22 -0500

    Added Map + Waypoints Script
    Also included new waypoint tile sprite.

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Map : MonoBehaviour
+{
+    public static Transform startPoint;
+    public static Transform endPoint;
+
+    private static Grid tileGrid;
+
+    // Awake is called when the script instance is loaded.
+    private void Awake() {
+        startPoint = transform.Find("Start");
+        endPoint = transform.Find("End");
+        tileGrid = gameObject.GetComponentInChildren<Grid>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            GetTile(Input.mousePosition);
+        }   
+    }
+
+    public static Vector3 GetTile(Vector3 mousePos) {
+        Debug.Log("Mouse pos: (" + mousePos.x + "," + mousePos.y + ")");
+        Vector3 tilePos = tileGrid.GetCellCenterWorld(tileGrid.WorldToCell(mousePos));
+        Debug.Log("Tile pos: (" + tilePos.x + "," + tilePos.y + ")");
+        return tilePos;
+    }
+
+
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public static Transform[] points;
+
+    // Awake is called when the script instance is loaded.
+    private void Awake() {
+        points = new Transform[transform.childCount]; //Set size of array to number of child objects (waypoints)
+        for(int i = 0; i < transform.childCount; i++) {
+            points[i] = transform.GetChild(i);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
